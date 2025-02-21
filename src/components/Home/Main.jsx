e0f9ee88e3e18a549c908578d4596fd11c0bfecd
jenish-ghoghari <jenishghoghari6@gmail.com>
2025-02-21 14:07:48 +0530

    fix issues

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -4,14 +4,21 @@ import HeroSection from "./HeroSection";
 import ProductCategory from "./ProductCategory";
 import Services from "../common/Services";
 import WhatWeDo from "./WhatWeDo";
+import { Helmet } from "react-helmet";
 
 const Main = () => {
-  
-
   return (
     <>
+      <Helmet>
+        <title>Home | SHC Pharmaceuticals</title>
+        <meta
+          name="description"
+          content="Collaborate with Leaders in Quality Third-Party Manufacturing"
+        />
+        <meta name="keywords" content="Ensuring Reliability and Consistency" />
+      </Helmet>
       <HeroSection />
-      <AboutUs/>
+      <AboutUs />
       <ProductCategory />
       <WhatWeDo />
       <Services />
